ba718d791eb64783944280e95331710565614230
Sakhi Akram <sakhiakram.github@outlook.com>
2026-07-08 12:43:44 +0500

    setting up the context and ai

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 require("dotenv").config();
+const { App } = require('@slack/bolt');
+const { GoogleGenerativeAI } = require('@google/generative-ai')
 
-const { App } = require("@slack/bolt");
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   appToken: process.env.SLACK_APP_TOKEN,
   socketMode: true
 });
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({model: 'gemini-1.5-flash'});
+
+const YDKJS_BOOKS = [
+  'https://raw.githubusercontent.com/getify/You-Dont-Know-JS/2nd-ed/get-started/README.md',
+  'https://raw.githubusercontent.com/getify/You-Dont-Know-JS/2nd-ed/scope-closures/README.md',
+  'https://raw.githubusercontent.com/getify/You-Dont-Know-JS/2nd-ed/objects-classes/README.md',
+  'https://raw.githubusercontent.com/getify/You-Dont-Know-JS/2nd-ed/types-grammar/README.md',
+  'https://raw.githubusercontent.com/getify/You-Dont-Know-JS/2nd-ed/sync-async/README.md',
+  'https://raw.githubusercontent.com/getify/You-Dont-Know-JS/2nd-ed/es-next-beyond/README.md',
+];
+
+let ydkjsContext = '';
+
+
 
 app.command("/ydkjs-explain", async ({ command, ack, respond }) => {
   const start = Date.now();
